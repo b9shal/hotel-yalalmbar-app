f6e409c3582694e06ee0531b5459c2a096cbcd56
b9shal <bishal.k03@gmail.com>
2022-02-08 18:14:46 +0545

    tags added

--- data/data.ts
+++ data/data.ts
@@ -7,41 +7,41 @@ export const itemList: Iitem[] = [
     itemName: "Momo",
     price: 100,
     photo: "https://b.zmtcdn.com/data/pictures/6/19658246/4ee4dfc657bc43a5e5ba0d62a0297fdd.png",
-    tags: [{ id: 1, tag: "#MithoMomo"},{ id: 2, tag: "#MithoMomo"}]
+    tags: [{ id: 1, tag: "#ChamlingMomo"},{ id: 2, tag: "#MithoMomo"}]
   },
   {
     id: 2,
     itemName: "Chowmin",
     price: 100,
     photo: "https://b.zmtcdn.com/data/pictures/6/19658246/4ee4dfc657bc43a5e5ba0d62a0297fdd.png",
-    tags: [{ id: 1, tag: "#MithoMomo"},{ id: 2, tag: "#MithoMomo"}]
+    tags: [{ id: 1, tag: "#ChamlingMomo"},{ id: 2, tag: "#MithoMomo"}]
   },
   {
     id: 3,
     itemName: "Sukuti",
     price: 100,
     photo: "https://www.seekpng.com/png/full/415-4154409_chili-momo.png",
-    tags: [{ id: 1, tag: "#MithoMomo"},{ id: 2, tag: "#MithoMomo"}]
+    tags: [{ id: 1, tag: "#ChamlingMomo"},{ id: 2, tag: "#MithoMomo"}]
   },
   {
     id: 4,
     itemName: "Sekuwa",
     price: 100,
     photo: "https://media.istockphoto.com/photos/veg-fried-momo-with-mayonnaise-served-over-a-rustic-wooden-background-picture-id1292635587?k=20&m=1292635587&s=612x612&w=0&h=Wv6Fwc98KivVsib1HUdmeYa59PfsrIhdq3U935j_B24=",
-    tags: [{ id: 1, tag: "#MithoMomo"},{ id: 2, tag: "#MithoMomo"}]
+    tags: [{ id: 1, tag: "#ChamlingMomo"},{ id: 2, tag: "#MithoMomo"}]
   },
   {
     id: 5,
     itemName: "Roast",
     price: 100,
     photo: "https://media-cdn.tripadvisor.com/media/photo-s/16/3e/a2/87/fried-momo.jpg",
-    tags: [{ id: 1, tag: "#MithoMomo"},{ id: 2, tag: "#MithoMomo"}]
+    tags: [{ id: 1, tag: "#ChamlingMomo"},{ id: 2, tag: "#MithoMomo"}]
   },
   {
     id: 6,
     itemName: "Chat-Pat",
     price: 100,
     photo: "https://i.pinimg.com/originals/b4/12/81/b4128152cbdae56c4eb9055748f3b1dd.png",
-    tags: [{ id: 1, tag: "#MithoMomo"},{ id: 2, tag: "#MithoMomo"}]
+    tags: [{ id: 1, tag: "#ChamlingMomo"},{ id: 2, tag: "#MithoMomo"}]
   }
 ]
